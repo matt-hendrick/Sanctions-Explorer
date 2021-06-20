b06996334966f65b1a4f575c1f082581cbe2ee9c
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-06-20 13:09:57 -0400

    Add: Home background image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //Components
 import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 //Pages
 import Home from './pages/Home/Home';
@@ -11,15 +11,11 @@ import Home from './pages/Home/Home';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          {/* <Route exact path="/trips/:tripID" component={Trip} />
-              <Route exact path="/demo" component={Demo} /> */}
-          <Route path="/" component={Home} />
-        </Switch>
-        <Footer />
-      </div>
+      <Navbar style={{ zIndex: '1' }} />
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+      {/* <Footer /> */}
     </Router>
   );
 }
