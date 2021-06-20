efbda7fcf2169ea5e7438a0ee223bdfc0eef57de
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-06-20 13:49:43 -0400

    Style: Improve home styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../../images/logo.png';
+import Logo from '../../images/logo.png';
 
 //Bootstrap
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,52 +8,54 @@ import Container from 'react-bootstrap/Container';
 
 function MyNavbar() {
   return (
-    <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img alt="Sanctions Explorer Logo" src={image} width="250px" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse
-          id="responsive-navbar-nav"
-          className="justify-content-end"
-        >
-          <Nav>
-            <Nav.Link
-              style={{
-                padding: '12px 20px 10px',
-              }}
-              href="./search"
-            >
-              Search
-            </Nav.Link>
-            <Nav.Link
-              style={{
-                padding: '12px 20px 10px',
-              }}
-              href="./analytics"
-            >
-              Analytics
-            </Nav.Link>
-            <Nav.Link
-              style={{
-                padding: '12px 20px 10px',
-              }}
-              href="./feedback"
-            >
-              Feedback
-            </Nav.Link>
-            <Nav.Link
-              style={{
-                padding: '12px 20px 10px',
-              }}
-              href="./about"
-            >
-              About
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar collapseOnSelect expand="md" style={{ backgroundColor: '#ffffff' }}>
+      <Navbar.Brand href="#home" style={{ paddingLeft: '10px' }}>
+        <img alt="Sanctions Explorer Logo" src={Logo} width="250px" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className="justify-content-end"
+      >
+        <Nav>
+          <Nav.Link
+            style={{
+              padding: '12px 20px 10px',
+              color: 'rgba(0,0,0,.7)',
+            }}
+            href="./search"
+          >
+            Search
+          </Nav.Link>
+          <Nav.Link
+            style={{
+              padding: '12px 20px 10px',
+              color: 'rgba(0,0,0,.7)',
+            }}
+            href="./analytics"
+          >
+            Analytics
+          </Nav.Link>
+          <Nav.Link
+            style={{
+              padding: '12px 20px 10px',
+              color: 'rgba(0,0,0,.7)',
+            }}
+            href="./feedback"
+          >
+            Feedback
+          </Nav.Link>
+          <Nav.Link
+            style={{
+              padding: '12px 20px 10px',
+              color: 'rgba(0,0,0,.7)',
+            }}
+            href="./about"
+          >
+            About
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
+import Logo from '../../images/logo.png';
+import SearchImage from '../../images/search.png';
+
+// Bootstrap
+import Form from 'react-bootstrap/Form';
 
 function Home() {
-  return <div className="home"></div>;
+  const [userSearch, setUserSearch] = useState('');
+
+  const handleUserSearchChange = (event) => {
+    const updatedUserSearch = event.target.value;
+    setUserSearch(updatedUserSearch);
+  };
+
+  return (
+    <div className="home">
+      <div className="d-flex align-items-center justify-content-center home-inner">
+        <div className="text-center">
+          <img className="logo" src={Logo} alt="Sanctions Explorer Logo" />
+          <p>Explore 28 years of sanctions data</p>
+          <Form aria-label="Search">
+            <div className="search-bar d-flex">
+              <input
+                className="form-control home-search"
+                type="text"
+                aria-label="Enter keyword to search OFAC, UN and EU data"
+                placeholder="Search OFAC, UN and EU"
+                value={userSearch}
+                onChange={handleUserSearchChange}
+              />
+              <div>
+                <button
+                  className="btn btn-primary"
+                  aria-label="Search Button"
+                  color="primary"
+                >
+                  <img src={SearchImage} alt="Search Icon" />
+                </button>
+              </div>
+            </div>
+          </Form>
+
+          <p className="mt-2">
+            18,927 entries across OFAC, UN and EU. Updated 10 days ago
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
